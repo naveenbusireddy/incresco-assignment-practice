5ae2da7a3b6444d4fffd70cb8a29da2d999648c3
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-04-27 13:06:37 +0530

    Upto Filter options

--- src/Components/ProductsPage/ProductsPage.jsx
+++ src/Components/ProductsPage/ProductsPage.jsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import ProductList from "../ProductList/ProductList";
 import Sort from "../Sort/Sort";
+import Filters from "../Filters/Filters";
 
-const ProductsPage = () => {
+import "./ProductsPage.css";
+
+const ProductsPage = () => { 
 
     const [products, setProducts] = useState();
     const [finalProducts, setFinalProducts] = useState();
     const [searchOption, setSearchOption] = useState();
     const [sortOption, setSortOption] = useState('priceLowToHigh');
+    const [filters, setFilters] = useState();
+    const [selectedFilter, setSelectedFilter] = useState({
+        Brand:[],
+        Color:[],
+        Gender:[]
+    });
 
     const getData = () => {
         axios.get(`https://demo7303877.mockable.io/`)
@@ -16,7 +25,8 @@ const ProductsPage = () => {
                 {
                     responseData.data.products=responseData.data.products.slice(0,9);
                     setFinalProducts(responseData.data.products);
-                    setProducts(responseData.data.products);
+                    setProducts(JSON.parse(JSON.stringify(responseData.data.products)));
+                    setFilters(JSON.parse(JSON.stringify(responseData.data.filters.primaryFilters)));
                 }                
             )
             
@@ -30,45 +40,67 @@ const ProductsPage = () => {
     }
 
     const getSortProductList = () => {
-        let copy=JSON.parse(JSON.stringify(finalProducts));
+        let productsListCopy=JSON.parse(JSON.stringify(finalProducts));
         if(sortOption === "priceLowToHigh")
         {
-            (copy.sort((a, b) => a["price"] - b["price"]));
-            setFinalProducts(copy);
+            (productsListCopy.sort((a, b) => a["price"] - b["price"]));
+            setFinalProducts(productsListCopy);
         }
         else if(sortOption === "priceHighToLow")
         {
-            (copy.sort((a, b) => b["price"] - a["price"]));
-            setFinalProducts(copy);            
+            (productsListCopy.sort((a, b) => b["price"] - a["price"]));
+            setFinalProducts(productsListCopy);            
         }
         else if(sortOption === "newest")
         {
-            (copy.sort((a, b) => b['year'] - a['year']));
-            setFinalProducts(copy);
+            (productsListCopy.sort((a, b) => b['year'] - a['year']));
+            setFinalProducts(productsListCopy);
         }
         else if(sortOption === 'oldest')
         {
-            (copy.sort((a, b) => a['year'] - b['year']));
-            setFinalProducts(copy);
+            (productsListCopy.sort((a, b) => a['year'] - b['year']));
+            setFinalProducts(productsListCopy);
         }
         else {
-            (copy.sort((a, b) => a[sortOption] - b[sortOption]));
-            setFinalProducts(copy);
+            (productsListCopy.sort((a, b) => a[sortOption] - b[sortOption]));
+            setFinalProducts(productsListCopy);
         }
         
     }
     useEffect(() => {
         if(finalProducts)
         getSortProductList()
-    },[onSortSelect])
+    },[sortOption])
 
+    const onSelectFilter = (event, filterType) => {
+        let selectedList =[] ;
+        if(event.target.checked)
+        {            
+            selectedList = [...selectedFilter[filterType], event.target.value];
+        }
+        else {
+            selectedList = [...selectedFilter[filterType]];  
+            selectedList.splice(selectedFilter[filterType].indexOf(event.target.value),1)
+        }
+        let currentFilter={};
+        currentFilter[filterType]=selectedList;
+        setSelectedFilter({...selectedFilter,...currentFilter});
+    }   
+
+    const selectedProductList = () => {
+        
+    }
+    useEffect(() => {
+        console.log(selectedFilter)
+    }, [selectedFilter]);
+    
     const onChangeHandler = (event) => {
         setSearchOption(event.target.value);
     }
     
     const searchedProductList = () => {
         
-        searchOption.length === 0 ? setFinalProducts(products) : setFinalProducts(products.filter((product) => product.productName.trim().toLowerCase().includes(searchOption.trim().toLowerCase())))
+        searchOption !== undefined ? setFinalProducts(products.filter((product) => product.productName.trim().toLowerCase().includes(searchOption.trim().toLowerCase()))) : setFinalProducts(products)
     }
     useEffect(() => {
         if(finalProducts)
@@ -76,12 +108,18 @@ const ProductsPage = () => {
     },[searchOption])
 
     return (
-        <>
-            <Sort onSelect={onSortSelect}/>
-            <input type="search" placeholder="Product Search" value={searchOption} onChange={onChangeHandler} />
-            {finalProducts && <ProductList ProductList={finalProducts} />}
-            {/* {subArray && <ProductList ProductList={subArray} />} */}
-        </>
+        <div className="grid-container">
+            <div className="searchSort">
+                <Sort onSelect={onSortSelect}/>
+                <input type="search" placeholder="Product Search" value={searchOption} onChange={onChangeHandler} />
+            </div>            
+            <div className="products">
+                {finalProducts && <ProductList ProductList={finalProducts} />}
+            </div>            
+            <div className="filters">
+            {filters && <Filters filters = {filters} onSelectFilter={onSelectFilter}/>}                
+            </div>
+        </div>
     )
 }
 export default ProductsPage;
